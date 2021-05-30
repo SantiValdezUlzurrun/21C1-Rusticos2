c1d9d7e4e206b8c02a6d230cc2e2927b86372185
Santiago Valdez <santiagoval1999@gmail.com>
2021-05-30 17:38:13 -0300

    feat: configuracion interactua con redis

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,18 @@ use crate::parser::parsear_int;
 use crate::redis::Redis;
 use std::env;
 use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 pub enum ArchivoError {
     ArchivoInexistenteError,
     ArchivoIncompletoError,
 }
-use std::io::prelude::*;
 
-use std::io::BufReader;
 #[derive(Debug)]
 pub struct Config {
     verbose: bool,
-    port: u32,
+    port: String,
     timeout: u32,
     dbfilename: String,
     logfile: String,
@@ -25,7 +26,7 @@ impl Config {
     fn new() -> Self {
         Config {
             verbose: false,
-            port: 8080,
+            port: "8080".to_string(),
             timeout: 0,
             dbfilename: "dump.rb".to_string(),
             logfile: "redis.log".to_string(),
@@ -50,6 +51,8 @@ fn obtener_configuracion(ruta_archivo: String) -> Result<Config, ArchivoError> {
             config.dbfilename = argumento[1].to_string();
         } else if argumento[0] == "logfile" {
             config.logfile = argumento[1].to_string();
+        } else if argumento[0] == "port" {
+            config.port = argumento[1].to_string();
         } else {
             let valor = match parsear_int(argumento[1].to_string()) {
                 Some(v) => v,
@@ -57,8 +60,6 @@ fn obtener_configuracion(ruta_archivo: String) -> Result<Config, ArchivoError> {
             };
             if argumento[0] == "verbose" {
                 config.verbose = valor == 1;
-            } else if argumento[1] == "port" {
-                config.port = valor;
             } else {
                 config.timeout = valor;
             }
@@ -79,8 +80,8 @@ fn main() {
     println!("{:?}", config);
 
     let host: &str = "127.0.0.1";
-    let port: &str = "8080";
-    let mut redis: Redis = Redis::new(host, port);
+
+    let mut redis: Redis = Redis::new(host, &config.port, config.verbose, config.timeout);
     match redis.iniciar() {
         Ok(_) => (),
         Err(_) => println!("Error al iniciar"),
