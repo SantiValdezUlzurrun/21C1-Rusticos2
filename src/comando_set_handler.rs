3d5a85996090bd1707ec153dad7cdb3e024b2be5
Santiago Valdez <santiagoval1999@gmail.com>
2021-06-10 19:30:15 -0300

    test: testing del set

--- src/comando_set_handler.rs
+++ src/comando_set_handler.rs
@@ -42,21 +42,25 @@ pub fn es_comando_set(comando: &String) -> bool {
 
 
 fn sadd(comando: &Vec<String>,  hash_map: Arc<Mutex<HashMap<String, TipoRedis>>>) -> ResultadoRedis {
-    let a_agregar = match hash_map.lock().unwrap().get(&comando[1]) {
+    let (a_agregar, cantidad_ingresada) = match hash_map.lock().unwrap().get(&comando[1]) {
         Some(TipoRedis::Set(set)) => aggregar_al_set(&comando[2..], &mut set.clone()),
         None => aggregar_al_set(&comando[2..], &mut HashSet::new()),
         _ => return ResultadoRedis::Error("WrongTypeError error al obtener el set, valor guardado en la clave no es un Set".to_string()),
     };
     hash_map.lock().unwrap().insert(comando[1].clone(), TipoRedis::Set(a_agregar));
-    ResultadoRedis::Int(comando[2..].len())
+    ResultadoRedis::Int(cantidad_ingresada)
 }
 
-fn aggregar_al_set(valores: &[String], set : &mut HashSet<String>) -> HashSet<String>{
-
+fn aggregar_al_set(valores: &[String], set : &mut HashSet<String>) -> (HashSet<String>, usize){
+    
+    let mut cantidad_ingresada = 0;
     for valor in valores.iter() {
-       set.insert(valor.clone()); 
+        if !set.contains(valor) {
+            set.insert(valor.clone());
+            cantidad_ingresada += 1;
+        }
     }
-    set.clone()
+    (set.clone(), cantidad_ingresada)
 }
 
 
@@ -91,21 +95,320 @@ fn smembers(comando: &Vec<String>,  hash_map: Arc<Mutex<HashMap<String, TipoRedi
 }
 
 fn srem(comando: &Vec<String>,  hash_map: Arc<Mutex<HashMap<String, TipoRedis>>>) -> ResultadoRedis { 
-    let a_agregar = match hash_map.lock().unwrap().get(&comando[1]) {
+    let (a_agregar, cantidad_eliminada) = match hash_map.lock().unwrap().get(&comando[1]) {
         Some(TipoRedis::Set(set)) => eliminar_del_set(&comando[2..], &mut set.clone()),
         None => return ResultadoRedis::Int(0),
         _ => return ResultadoRedis::Error("WrongTypeError error al obtener el set, valor guardado en la clave no es un Set".to_string()),
     };
 
     hash_map.lock().unwrap().insert(comando[1].clone(), TipoRedis::Set(a_agregar));
-    ResultadoRedis::Int(comando[2..].len())
+    ResultadoRedis::Int(cantidad_eliminada)
 }
 
 
-fn eliminar_del_set(valores: &[String], set : &mut HashSet<String>) -> HashSet<String>{
-
+fn eliminar_del_set(valores: &[String], set : &mut HashSet<String>) -> (HashSet<String>, usize){
+    let mut cantidad_eliminada = 0;
     for valor in valores.iter() {
-       set.remove(valor); 
+        if set.contains(valor) {
+            set.remove(valor);  
+            cantidad_eliminada += 1;
+        }
     }
-    set.clone()
+    (set.clone(), cantidad_eliminada)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    //sadd
+    #[test]
+    fn sadd_cuando_se_envia_una_clave_que_no_esta_esta_se_crea_y_se_almacena_correctamente() {
+        let hash: HashMap<String, TipoRedis> = HashMap::new(); 
+        let vector = vec!["SADD".to_string(), "miClave".to_string(), "miValor".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = sadd(&vector, Arc::clone(&h)); 
+        
+        
+
+        assert_eq!(
+            ResultadoRedis::Int(1), 
+            resultado, 
+        );
+        
+        let mut set = HashSet::new();
+        set.insert("miValor".to_string());
+        assert_eq!(
+            h.lock().unwrap().get(&"miClave".to_string()).unwrap(), 
+            &TipoRedis::Set(set), 
+        );
+    }
+
+
+    #[test]
+    fn sadd_cuando_se_envia_un_valor_que_esta_repetido_esta_no_se_almacena() {
+        let hash: HashMap<String, TipoRedis> = HashMap::new(); 
+        let vector = vec!["SADD".to_string(), "miClave".to_string(), "miValor".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+
+        sadd(&vector, Arc::clone(&h)); 
+        let resultado = sadd(&vector, Arc::clone(&h)); 
+        
+        assert_eq!(
+            ResultadoRedis::Int(0), 
+            resultado, 
+        ); 
+    }
+    
+    #[test]
+    fn sadd_cuando_se_envia_una_clave_invalida_se_envia_el_error_adecuado() {
+        let mut hash: HashMap<String, TipoRedis> = HashMap::new(); 
+        hash.insert("miClave".to_string(), TipoRedis::Str("unString".to_string()));
+        let vector = vec!["SADD".to_string(), "miClave".to_string(), "miValor".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = sadd(&vector, Arc::clone(&h)); 
+
+        assert_eq!(
+            ResultadoRedis::Error("WrongTypeError error al obtener el set, valor guardado en la clave no es un Set".to_string()), 
+            resultado, 
+        );
+    }
+
+    //scard
+    #[test]
+    fn scard_cuando_se_envia_una_clave_que_no_esta_se_devuelve_0_cardinalidad() {
+        let hash: HashMap<String, TipoRedis> = HashMap::new();  
+        let vector = vec!["SCARD".to_string(), "miClave".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = scard(&vector, Arc::clone(&h)); 
+
+        assert_eq!(
+            ResultadoRedis::Int(0), 
+            resultado, 
+        );
+
+    }
+
+    #[test]
+    fn scard_cuando_se_envia_una_clave_que_posee_dos_elementos_se_devuelve_2_de_cardinalidad() {
+        let mut hash: HashMap<String, TipoRedis> = HashMap::new();  
+        
+        let mut set = HashSet::new();
+        set.insert("miValor".to_string());
+        set.insert("otroValor".to_string());
+
+        hash.insert("miClave".to_string(), TipoRedis::Set(set));
+        let vector = vec!["SCARD".to_string(), "miClave".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = scard(&vector, Arc::clone(&h)); 
+
+        assert_eq!(
+            ResultadoRedis::Int(2), 
+            resultado, 
+        );
+
+    }
+
+    #[test]
+    fn scard_cuando_se_envia_una_clave_invalida_se_envia_el_error_adecuado() {
+        let mut hash: HashMap<String, TipoRedis> = HashMap::new(); 
+        hash.insert("miClave".to_string(), TipoRedis::Str("unString".to_string()));
+        let vector = vec!["SCARD".to_string(), "miClave".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = scard(&vector, Arc::clone(&h)); 
+
+        assert_eq!(
+            ResultadoRedis::Error("WrongTypeError error al obtener el set, valor guardado en la clave no es un Set".to_string()), 
+            resultado, 
+        );
+    }
+    
+    //sismember
+    #[test]
+    fn sismember_cuando_se_envia_una_clave_que_no_esta_devuelve_0_ya_que_no_pertenece() {
+        let hash: HashMap<String, TipoRedis> = HashMap::new();  
+        let vector = vec!["SISMEMBER".to_string(), "miClave".to_string(), "miValor".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = sismember(&vector, Arc::clone(&h)); 
+
+        assert_eq!(
+            ResultadoRedis::Int(0), 
+            resultado, 
+        );
+
+    }
+
+    #[test]
+    fn sismember_cuando_se_envia_una_clave_que_posee_dos_elementos_y_se_pregunta_si_uno_de_ellos_pertenece_se_devuelve_1_de_true() {
+        let mut hash: HashMap<String, TipoRedis> = HashMap::new();  
+        
+        let mut set = HashSet::new();
+        set.insert("miValor".to_string());
+        set.insert("otroValor".to_string());
+
+        hash.insert("miClave".to_string(), TipoRedis::Set(set)); 
+        let vector = vec!["SISMEMBER".to_string(), "miClave".to_string(), "miValor".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = sismember(&vector, Arc::clone(&h)); 
+
+        assert_eq!(
+            ResultadoRedis::Int(1), 
+            resultado, 
+        );
+
+    }
+
+    #[test]
+    fn sismember_cuando_se_envia_una_clave_invalida_se_envia_el_error_adecuado() {
+        let mut hash: HashMap<String, TipoRedis> = HashMap::new(); 
+        hash.insert("miClave".to_string(), TipoRedis::Str("unString".to_string()));
+        let vector = vec!["SISMEMBER".to_string(), "miClave".to_string(), "miValor".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = sismember(&vector, Arc::clone(&h)); 
+
+        assert_eq!(
+            ResultadoRedis::Error("WrongTypeError error al obtener el set, valor guardado en la clave no es un Set".to_string()), 
+            resultado, 
+        );
+    }
+    //smembers
+    #[test]
+    fn smembers_cuando_se_envia_una_clave_que_no_esta_devuelve_un_vector_vacio() {
+        let hash: HashMap<String, TipoRedis> = HashMap::new();  
+        let vector = vec!["SMEMBERS".to_string(), "miClave".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = smembers(&vector, Arc::clone(&h)); 
+
+        assert_eq!(
+            ResultadoRedis::Vector(vec![]), 
+            resultado, 
+        );
+
+    } 
+
+    #[test]
+    fn smembers_cuando_se_envia_una_clave_invalida_se_envia_el_error_adecuado() {
+        let mut hash: HashMap<String, TipoRedis> = HashMap::new(); 
+        hash.insert("miClave".to_string(), TipoRedis::Str("unString".to_string()));
+        let vector = vec!["SMEMBERS".to_string(), "miClave".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = smembers(&vector, Arc::clone(&h)); 
+
+        assert_eq!(
+            ResultadoRedis::Error("WrongTypeError error al obtener el set, valor guardado en la clave no es un Set".to_string()), 
+            resultado, 
+        );
+    }
+
+    //srem
+    #[test]
+    fn srem_cuando_se_envia_una_clave_que_no_esta_no_se_elimina_ningun_valor() {
+        let hash: HashMap<String, TipoRedis> = HashMap::new(); 
+        let vector = vec!["SREM".to_string(), "miClave".to_string(), "miValor".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = srem(&vector, Arc::clone(&h)); 
+        
+        
+
+        assert_eq!(
+            ResultadoRedis::Int(0), 
+            resultado, 
+        );
+    }
+    
+    #[test]
+    fn srem_cuando_se_envia_una_clave_que_posee_dos_elementos_y_se_elimina_uno_este_no_se_encuentra() {
+        let mut hash: HashMap<String, TipoRedis> = HashMap::new();  
+        
+        let mut set = HashSet::new();
+        set.insert("miValor".to_string());
+        set.insert("otroValor".to_string());
+
+        hash.insert("miClave".to_string(), TipoRedis::Set(set));
+        let vector = vec!["SREM".to_string(), "miClave".to_string(), "miValor".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = srem(&vector, Arc::clone(&h)); 
+
+        assert_eq!(
+            ResultadoRedis::Int(1), 
+            resultado, 
+        );
+        
+        let mut set = HashSet::new();
+        set.insert("otroValor".to_string());
+        assert_eq!(
+            h.lock().unwrap().get(&"miClave".to_string()).unwrap(), 
+            &TipoRedis::Set(set), 
+        );
+    }
+    
+    #[test]
+    fn srem_cuando_se_envia_una_clave_que_posee_dos_elementos_y_se_eliminan_solo_uno_dos_veces_no_se_encuentra_el_restante() {
+        let mut hash: HashMap<String, TipoRedis> = HashMap::new();  
+        
+        let mut set = HashSet::new();
+        set.insert("miValor".to_string());
+        set.insert("otroValor".to_string());
+
+        hash.insert("miClave".to_string(), TipoRedis::Set(set));
+        let vector = vec!["SREM".to_string(), "miClave".to_string(), "miValor".to_string(), "miValor".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = srem(&vector, Arc::clone(&h)); 
+
+        assert_eq!(
+            ResultadoRedis::Int(1), 
+            resultado, 
+        );
+        
+        let mut set = HashSet::new();
+        set.insert("otroValor".to_string());
+        assert_eq!(
+            h.lock().unwrap().get(&"miClave".to_string()).unwrap(), 
+            &TipoRedis::Set(set), 
+        );
+    }
+    
+    #[test]
+    fn srem_cuando_se_envia_una_clave_invalida_se_envia_el_error_adecuado() {
+        let mut hash: HashMap<String, TipoRedis> = HashMap::new(); 
+        hash.insert("miClave".to_string(), TipoRedis::Str("unString".to_string()));
+        let vector = vec!["SREM".to_string(), "miClave".to_string(), "miValor".to_string()];
+        
+        let h = Arc::new(Mutex::new(hash));
+
+        let resultado = srem(&vector, Arc::clone(&h)); 
+
+        assert_eq!(
+            ResultadoRedis::Error("WrongTypeError error al obtener el set, valor guardado en la clave no es un Set".to_string()), 
+            resultado, 
+        );
+    }
 }
