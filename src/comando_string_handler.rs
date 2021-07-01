0f5b195b803c071e09b6062a16d02023439e34da
MauricioBuzzone <mbuzzone@fi.uba.ar>
2021-07-01 18:21:11 -0300

    feat: comando decrby y incrby

--- src/comando_string_handler.rs
+++ src/comando_string_handler.rs
@@ -5,9 +5,7 @@ use std::sync::{Arc, Mutex};
 
 /*
 Comando Lista faltantes:
-+ decrby
 + getset
-+ incrby
 + mget
 + mset
 */
@@ -120,6 +118,51 @@ fn strlen(comando: &mut ComandoInfo, bdd: Arc<Mutex<BaseDeDatos>>) -> ResultadoR
         _ => ResultadoRedis::Error("StrLen error al obtener la clave".to_string()),
     }
 }
+
+fn operar_sobre_int(comando: &mut ComandoInfo, bdd: Arc<Mutex<BaseDeDatos>>, f: fn(i32,i32) -> i32) -> ResultadoRedis{
+    let clave = match comando.get_clave() {
+        Some(c) => c,
+        None => return ResultadoRedis::Error("ClaveError no se encontro una clave".to_string()),
+    };
+
+    let valor = match bdd.lock().unwrap().obtener_valor(&clave) {
+        Some(TipoRedis::Str(valor)) => valor.clone(),
+        None => "0".to_string(),
+        _ => return ResultadoRedis::Error("WRONGTYPE".to_string()),
+    };
+
+    let mut num = match valor.parse::<i32>() {
+        Ok(n) => n,
+        Err(_) => return ResultadoRedis::Error("Valor no es un int".to_string()),
+    };
+       
+    let param = match comando.get_parametro() {
+        Some(p) => p,
+        None => return ResultadoRedis::Error("ParametroError no se encontro un parametro".to_string()),
+    };
+       
+    let param = match param.parse::<i32>() {
+        Ok(p) => p,
+        Err(_) => return ResultadoRedis::Error("Parametro no es un int".to_string()),
+    };
+
+    num = f(num,param);
+    bdd.lock().unwrap().guardar_valor(clave,TipoRedis::Str(num.to_string()));
+
+    ResultadoRedis::BulkStr(num.to_string())
+}
+
+
+#[allow(dead_code)]
+fn decrby(comando: &mut ComandoInfo, bdd: Arc<Mutex<BaseDeDatos>>) -> ResultadoRedis {
+    operar_sobre_int(comando,bdd, |a,b| a-b)
+}
+
+#[allow(dead_code)]
+fn incrby(comando: &mut ComandoInfo, bdd: Arc<Mutex<BaseDeDatos>>) -> ResultadoRedis {
+    operar_sobre_int(comando,bdd, |a,b| a+b)
+}
+
 
 #[cfg(test)]
 mod tests {
@@ -273,6 +316,148 @@ mod tests {
         assert_eq!(
             ResultadoRedis::Error("StrLen error al obtener la clave".to_string()),
             strlen(&mut comando, Arc::new(Mutex::new(bdd)))
+        );
+    }
+
+    #[test]
+    fn decrby_resta_correcatemente_un_valor_entero_a_una_clave_parseable() {
+        let mut bdd: BaseDeDatos = BaseDeDatos::new("eliminame.txt".to_string());
+        bdd.guardar_valor("miClave".to_string(), TipoRedis::Str("1".to_string()));
+        let mut comando = ComandoInfo::new(vec!["decrby".to_string(),"miClave".to_string(),"1".to_string()]);
+
+        assert_eq!(
+            ResultadoRedis::BulkStr("0".to_string()),
+            decrby(&mut comando, Arc::new(Mutex::new(bdd)))
+        );
+    }
+
+    #[test]
+    fn decrby_resta_correcatemente_un_valor_entero_a_una_clave_negativa_parseable() {
+        let mut bdd: BaseDeDatos = BaseDeDatos::new("eliminame.txt".to_string());
+        bdd.guardar_valor("miClave".to_string(), TipoRedis::Str("-10".to_string()));
+        let mut comando = ComandoInfo::new(vec!["decrby".to_string(),"miClave".to_string(),"1".to_string()]);
+
+        assert_eq!(
+            ResultadoRedis::BulkStr("-11".to_string()),
+            decrby(&mut comando, Arc::new(Mutex::new(bdd)))
+        );
+    }
+
+    #[test]
+    fn decrby_resta_correcatemente_un_valor_negativo_a_una_clave_parseable() {
+        let mut bdd: BaseDeDatos = BaseDeDatos::new("eliminame.txt".to_string());
+        bdd.guardar_valor("miClave".to_string(), TipoRedis::Str("10".to_string()));
+        let mut comando = ComandoInfo::new(vec!["decrby".to_string(),"miClave".to_string(),"-1".to_string()]);
+
+        assert_eq!(
+            ResultadoRedis::BulkStr("11".to_string()),
+            decrby(&mut comando, Arc::new(Mutex::new(bdd)))
+        );
+    }
+
+    #[test]
+    fn decrby_setea_correcatemente_un_valor_entero_a_una_clave_inexistente() {
+        let bdd: BaseDeDatos = BaseDeDatos::new("eliminame.txt".to_string());
+        let mut comando = ComandoInfo::new(vec!["decrby".to_string(),"miClave".to_string(),"-1".to_string()]);
+
+        assert_eq!(
+            ResultadoRedis::BulkStr("1".to_string()),
+            decrby(&mut comando, Arc::new(Mutex::new(bdd)))
+        );
+    }
+
+    #[test]
+    fn decrby_devuelve_error_un_valor_entero_a_una_clave_inparseable() {
+        let mut bdd: BaseDeDatos = BaseDeDatos::new("eliminame.txt".to_string());
+         bdd.guardar_valor("miClave".to_string(), TipoRedis::Lista(LinkedList::new()));
+        let mut comando = ComandoInfo::new(vec!["decrby".to_string(),"miClave".to_string(),"-1".to_string()]);
+
+        assert_eq!(
+            ResultadoRedis::Error("WRONGTYPE".to_string()),
+            decrby(&mut comando, Arc::new(Mutex::new(bdd)))
+        );
+    }
+
+    #[test]
+    fn decrby_devuelve_error_un_valor_erroneo_a_una_clave_parseable() {
+        let mut bdd: BaseDeDatos = BaseDeDatos::new("eliminame.txt".to_string());
+         bdd.guardar_valor("miClave".to_string(), TipoRedis::Str("1".to_string()));
+        let mut comando = ComandoInfo::new(vec!["decrby".to_string(),"miClave".to_string(),"a".to_string()]);
+
+        assert_eq!(
+            ResultadoRedis::Error("Parametro no es un int".to_string()),
+            decrby(&mut comando, Arc::new(Mutex::new(bdd)))
+        );
+    }
+
+    #[test]
+    fn incrby_resta_correcatemente_un_valor_entero_a_una_clave_parseable() {
+        let mut bdd: BaseDeDatos = BaseDeDatos::new("eliminame.txt".to_string());
+        bdd.guardar_valor("miClave".to_string(), TipoRedis::Str("1".to_string()));
+        let mut comando = ComandoInfo::new(vec!["incrby".to_string(),"miClave".to_string(),"1".to_string()]);
+
+        assert_eq!(
+            ResultadoRedis::BulkStr("2".to_string()),
+            incrby(&mut comando, Arc::new(Mutex::new(bdd)))
+        );
+    }
+
+    #[test]
+    fn incrby_resta_correcatemente_un_valor_entero_a_una_clave_negativa_parseable() {
+        let mut bdd: BaseDeDatos = BaseDeDatos::new("eliminame.txt".to_string());
+        bdd.guardar_valor("miClave".to_string(), TipoRedis::Str("-10".to_string()));
+        let mut comando = ComandoInfo::new(vec!["incrby".to_string(),"miClave".to_string(),"1".to_string()]);
+
+        assert_eq!(
+            ResultadoRedis::BulkStr("-9".to_string()),
+            incrby(&mut comando, Arc::new(Mutex::new(bdd)))
+        );
+    }
+
+    #[test]
+    fn incrby_resta_correcatemente_un_valor_negativo_a_una_clave_parseable() {
+        let mut bdd: BaseDeDatos = BaseDeDatos::new("eliminame.txt".to_string());
+        bdd.guardar_valor("miClave".to_string(), TipoRedis::Str("10".to_string()));
+        let mut comando = ComandoInfo::new(vec!["incrby".to_string(),"miClave".to_string(),"-1".to_string()]);
+
+        assert_eq!(
+            ResultadoRedis::BulkStr("9".to_string()),
+            incrby(&mut comando, Arc::new(Mutex::new(bdd)))
+        );
+    }
+
+    #[test]
+    fn incrby_setea_correcatemente_un_valor_entero_a_una_clave_inexistente() {
+        let bdd: BaseDeDatos = BaseDeDatos::new("eliminame.txt".to_string());
+        let mut comando = ComandoInfo::new(vec!["incrby".to_string(),"miClave".to_string(),"1".to_string()]);
+
+        assert_eq!(
+            ResultadoRedis::BulkStr("1".to_string()),
+            incrby(&mut comando, Arc::new(Mutex::new(bdd)))
+        );
+    }
+
+    #[test]
+    fn incrby_devuelve_error_un_valor_entero_a_una_clave_inparseable() {
+        let mut bdd: BaseDeDatos = BaseDeDatos::new("eliminame.txt".to_string());
+         bdd.guardar_valor("miClave".to_string(), TipoRedis::Lista(LinkedList::new()));
+        let mut comando = ComandoInfo::new(vec!["incrby".to_string(),"miClave".to_string(),"5".to_string()]);
+
+        assert_eq!(
+            ResultadoRedis::Error("WRONGTYPE".to_string()),
+            incrby(&mut comando, Arc::new(Mutex::new(bdd)))
+        );
+    }
+
+    #[test]
+    fn incrby_devuelve_error_un_valor_erroneo_a_una_clave_parseable() {
+        let mut bdd: BaseDeDatos = BaseDeDatos::new("eliminame.txt".to_string());
+         bdd.guardar_valor("miClave".to_string(), TipoRedis::Str("1".to_string()));
+        let mut comando = ComandoInfo::new(vec!["incrby".to_string(),"miClave".to_string(),"a".to_string()]);
+
+        assert_eq!(
+            ResultadoRedis::Error("Parametro no es un int".to_string()),
+            incrby(&mut comando, Arc::new(Mutex::new(bdd)))
         );
     }
 }
